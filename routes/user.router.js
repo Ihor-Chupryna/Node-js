daeb2d38ec614ece0f187dad6666c3feb6a06eca
ihor <igchup1985@gmail.com>
2022-06-21 17:54:31 +0300

    mongoose and middleware

---
+++ routes/user.router.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const { userController } = require('../controlers');
+const { commonMiddleware, userMiddleware } = require("../middlewares");
+
+router.get('/', userController.findUsers);
+router.post('/', userMiddleware.isUserValidForCreate,
+    userMiddleware.isUserUniq,
+    userController.createUser);
+
+router.get('/:id', commonMiddleware.isIdValid,
+    userMiddleware.isUserPresent,
+    userController.getUserById);
+router.put('/:id', commonMiddleware.isIdValid,
+    userMiddleware.isUserValidForUpdate,
+    userMiddleware.isUserPresent,
+    userController.updateUserById);
+router.delete('/:id', commonMiddleware.isIdValid,
+    userMiddleware.isUserPresent,
+    userController.deleteUserById);
+
+module.exports = router;
